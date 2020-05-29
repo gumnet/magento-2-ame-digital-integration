308e50765b87b61e0db20fe911a669c916e30f0f
Gustavo Ulyssea <gustavo.ulyssea@gmail.com>
2020-05-28 23:16:16 -0300

    Validator validate-uuid

--- view/adminhtml/web/js/systemxmlvalidation.js
+++ view/adminhtml/web/js/systemxmlvalidation.js
@@ -7,5 +7,11 @@ require([
             'validate-number-0-100', function (v) {
                 return (v <= 100 && v >= 0);
             }, $.mage.__('Value must be between 0 and 100'));
+        $.validator.addMethod(
+            'validate-uuid', function (v) {
+                var pattern = new RegExp('^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$','i')
+                return (pattern.test(v));
+            }, $.mage.__('Value must be a valid UUID'));
+
     }
 );
